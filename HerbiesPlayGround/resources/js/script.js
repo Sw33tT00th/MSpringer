b4af8d59a284d0bcc909b38060aec5261988f368
herbieduah <had37@nau.edu>
2016-03-22 03:01:10 -0700

    no sleeppp
    coming along great, wish i had more time. CSS IS MESSY DON’T TOUCH YET

--- HerbiesPlayGround/resources/js/script.js
+++ HerbiesPlayGround/resources/js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    $('#slider').leanSlider();
+ /* For the sticky navigation */
+    $('.righthere').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('peekaboo');
+            $('div.girls').removeClass('banner');
+            $('div.girls').addClass('nobanner');
+             $('img.girllogo').removeClass('home');
+            $('img.girllogo').addClass('logo');
+           
+        } else {
+            $('nav').removeClass('peekaboo');
+             $('div.girls').removeClass('nobanner');
+            $('div.girls').addClass('banner');
+            $('img.girllogo').removeClass('logo');
+            $('img.girllogo').addClass('home');
+        }
+    }, {
+      offset: '10px'
+    });
+    
+    
+    
 });
