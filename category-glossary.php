def10f22b6c5c6521d0879026c6e4218812aa77f
John Loudon <jsl245@nau.edu>
2016-04-21 09:46:22 -0700

    people are now included in the glossary as intended

--- category-glossary.php
+++ category-glossary.php
@@ -29,53 +29,71 @@ get_header(); ?>
 						<header class="page-header">
 							<h1 class="page-title">Glossary</h1>
 						</header><!-- .page-header -->
-			<div class="col-md-8">
-			<?php
+							<div class="col-md-8">
+							<?php
 
-			query_posts( array ( 'category_name' => 'glossary', 'posts_per_page' => -1, 'order' => 'ASC', 'orderby' => 'title' ) );
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+							query_posts( array ( 'category_name' => 'glossary', 'posts_per_page' => -1, 'order' => 'ASC', 'orderby' => 'title' ) );
+							/* Start the Loop */
+							while ( have_posts() ) : the_post();
 
-				$title = get_the_title();
-				$link = get_the_permalink();
-				$letter = substr($title, 0, 1);
-				$all_letters[$counter] = $letter;
-				$all_titles[$counter] = $title;
-				$all_links[$counter] = $link;
-				$counter += 1;
+								$title = get_the_title();
+								$link = get_the_permalink();
+								$letter = substr($title, 0, 1);
+								$all_letters[$counter] = $letter;
+								$all_titles[$counter] = $title;
+								$all_links[$counter] = $link;
+								$counter += 1;
 
-			endwhile; 
+							endwhile; 
 
-			$used_letters = array();
-			for ($i = 0; $i < count($all_titles); $i += 1) {
-				# Only add new letters to list
-				if(!in_array($all_letters[$i], $used_letters)) {
-					echo '<h3 class="glossary-letters">' . $all_letters[$i] . '</h3>';
-					array_push($used_letters, $all_letters[$i]);
-				}
-				# Add every post link
-				echo '<a class="aggregated-link" href="' . $all_links[$i] . '" title="' . $all_titles[$i] . '">' . $all_titles[$i] . '</a><br />';
-			}
-			echo '<div class="space"><div>';
-			
-			wp_reset_query();
+							$used_letters = array();
+							for ($i = 0; $i < count($all_titles); $i += 1) {
+								# Only add new letters to list
+								if(!in_array($all_letters[$i], $used_letters)) {
+									echo '<h3 class="glossary-letters">' . $all_letters[$i] . '</h3>';
+									array_push($used_letters, $all_letters[$i]);
+								}
+								# Add every post link
+								echo '<a class="aggregated-link" href="' . $all_links[$i] . '" title="' . $all_titles[$i] . '">' . $all_titles[$i] . '</a><br />';
+							}
+							echo '<div class="space"></div>';
+							
+							wp_reset_query();
 
-		else :
+						else :
 
+							get_template_part( 'template-parts/content', 'none' );
 
-						get_template_part( 'template-parts/content', 'none' );
+						endif;
 
+						?>
+						</div>
+						<div class="col-md-4">
+							<h3 class="people-header">People</h3>
 
-		endif;
+							<?php
 
-		 ?>
-		 </div>
-		 <div class="col-md-4">
-		 	
-		 </div>
+							$counter = 0;
+							$all_titles = array();
+							$all_links = array();
+							query_posts( array ( 'category_name' => 'people', 'posts_per_page' => -1 ) );
+							while( have_posts() ) : the_post();
 
+								$title = get_the_title();
+								$link = get_the_permalink();
+								$all_titles[$counter] = $title;
+								$all_links[$counter] = $link;
+								$counter += 1;
 
+							endwhile;
 
+							for ($i = 0; $i < count($all_titles); $i += 1) {
+								echo '<a class="aggregated-link" href="' . $all_links[$i] . '" title="' . $all_titles[$i] . '">' . $all_titles[$i] . '</a><br />';
+							}
+
+							wp_reset_query();
+							?>
+						</div>
 					</main><!-- #main -->
 				</div>
 			</div>
